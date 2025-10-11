630ff92e7d6decd7a8d268691f848d5d4bbd020c
Truong Duc Hoang <tantiennguyen2404@gmail.com>
2025-10-11 11:48:45 +0700

    Done BTTH01

---
+++ BTTH_01/BTH01_Bai02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "***************************" << endl;
+    cout << "* Bai thuc hanh 1         *" << endl;
+    cout << "* Mon: CSLT               *" << endl;
+    cout << "* MSSV: 2251012064        *" << endl;
+    cout << "***************************" << endl;
+    return 0;
+}
---
+++ BTTH_01/BTH01_Bai06.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout << "Xin chao, ten toi la Hoang!" << endl;
+    cout << "Ten ban la gi? ";
+    string username;
+    cin >> username;
+    cout << "Chao ban " << username << "!";
+    return 0;
+}
